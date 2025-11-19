903a3d4117ac2651d796483e9c222ebe9866c1b3
soban <sobanfarooq3@gmail.com>
2025-11-19 18:04:51 +0500

    updated delete + label dropdown

--- src/views/livewire/livewire-tagify.blade.php
+++ src/views/livewire/livewire-tagify.blade.php
@@ -103,25 +103,50 @@
     }">
 
         <input type="text" x-ref="tagInput" value='{{ $this->getModelTags() }}'>
-        <div x-ref="panel" class="tagify__dropdown tagify__dropdown--text absolute left-0 mt-2 px-2 rounded-md" x-show="openDropdown" x-transition.origin.top.left x-on:click.outside="close()"
-            style="display: none; !important;">
-            <!-- Added flex and flex-col classes -->
-            <div class="tagify__dropdown__item flex cursor-pointer px-4 py-2.5 my-1 text-left text-sm hover:bg-gray-200 disabled:text-gray-500 items-center">
-                <a x-on:click="deleteTag()"
-                    class="gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md mb-2">
-                    Delete
-                </a>
+        <div x-ref="panel"
+             x-show="openDropdown"
+             x-transition.opacity.duration.150ms
+             x-on:click.outside="close()"
+             style="display: none;"
+             class="tagify__dropdown absolute left-0 z-50 w-full">
+
+            <div class="tagify__dropdown__wrapper">
+                <button x-on:click="deleteTag()"
+                        class="tagify__dropdown__item w-full text-left flex items-center gap-2.5 group transition-colors duration-150 hover:bg-[var(--tag-remove-bg)]">
+
+                    <div class="text-gray-400 group-hover:text-[var(--tag-remove-btn-bg--hover)] transition-colors">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                            <path fill-rule="evenodd"
+                                  d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.1499.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149-.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 001.5.06l.3-7.5z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+
+                    <span class="font-medium text-xs text-gray-600 group-hover:text-[var(--tag-remove-btn-bg--hover)]">
+                        Delete Tag
+                    </span>
+                </button>
+
+                <div class="h-px w-full bg-gray-100 my-1"></div>
+
+                <div class="px-3 py-2">
+                    <p class="text-[10px] uppercase tracking-widest text-gray-400 font-bold mb-2">
+                        Label Color
+                    </p>
+
+                    <div class="flex flex-wrap gap-1.5">
+                        @foreach ($configuration['colors'] as $color)
+                            <button x-on:click="changeColor('{{ $color }}')"
+                                    class="w-6 h-6 rounded-full shadow-sm ring-1 ring-gray-900/5 
+                                 hover:scale-110 hover:ring-2 hover:ring-offset-1 hover:ring-[var(--dropdown-item-text--hover)] 
+                                 transition-all duration-200 ease-out focus:outline-none"
+                                    style="background: {{ $color }};"
+                                    title="{{ $color }}">
+                            </button>
+                        @endforeach
+                    </div>
+                </div>
             </div>
-
-            <div class="flex pl-2 pb-2 my-3" style="flex-wrap: wrap; margin-bottom: 10px">
-                @foreach ($configuration['colors'] as $color)
-                    <div x-on:click="changeColor('{{ $color }}')" class="cursor-pointer ml-1 mb-1 color-container"
-                    style="background: {{ $color }};"></div>
-                @endforeach
-            </div>
-
-
-            <!-- Add more flex items here -->
         </div>
     </div>
 
